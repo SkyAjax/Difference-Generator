c9e0011cbe6e7bd128b193f208ad02562cb9fbe4
Sergei Ruban <sergei24ruban@gmail.com>
2022-10-25 19:32:24 +0300

    fix eslint

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,14 +1,13 @@
-import genDiff from '../src/gendiff_engine';
+import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
-import path from 'path';
-import { dirname } from 'path';
+import genDiff from '../src/gendiff_engine.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 
 test('gendiff', () => {
-  expect(genDiff(getFixturePath('file1.json'),getFixturePath('file2.json'))).toEqual(`{
+  expect(genDiff(getFixturePath('file1.json'), getFixturePath('file2.json'))).toEqual(`{
   - follow: false
     host: hexlet.io
   - proxy: 123.234.53.22
